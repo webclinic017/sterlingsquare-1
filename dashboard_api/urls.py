5852e164f085dc48fadf7761e8282def1d799666
5500pradeepsingh <5500pradeepsingh@gmail.com>
2021-08-11 16:28:21 +0530

    add sector api

--- dashboard_api/urls.py
+++ dashboard_api/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('dashboard_api_login/', views.DashboardApiLogin.as_view()),
     path('dashboard_api_get_buying_power/', views.DashboardApiGetBuyingPower.as_view()),
     path('dashboard_api_get_realised_unrealised_gain_loss/', views.DashboardApiGetRealisedUnrealisedGainLoss.as_view()),
+    # path('dashboard_api_portfolio_performance_stock/', views.DashboardApiPortfolioPerformanceStock.as_view()),
+    path('dashboard_api_sectorwise_data/', views.DashboardApiSectorWiseData.as_view()),
+    path('dashboard_api_testing/', views.DashboardApiTesting.as_view()),
 
 ]
--- dashboard_api/views.py
+++ dashboard_api/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated
 from accounts.models import *
 from .dashboard_api_serializer import *
 from rest_framework.permissions import AllowAny
+from stock_react_api.core.models import *
 
 
 # from rest_framework.authentication import TokenAuthentication, BasicAuthentication
@@ -96,3 +97,151 @@ class DashboardApiGetBuyingPower(APIView):
         except:
             temp = [{"message": "not found"}]
             return Response(temp, status=status.HTTP_404_NOT_FOUND)
+
+
+import pyrebase
+import json
+import logging
+
+# class DashboardApiPortfolioPerformanceStock(APIView):
+#     # permission_classes = [IsAuthenticated]
+#
+#     def post(self, request):
+#         auth = request.headers.get('Authorization')
+#         token = Token.objects.get(key=auth.replace("Token ", ""))
+#         user_id = token.user_id
+#         portfolio_name = request.data['portfolio_name']
+#         my_portfolio = portfolio.objects.filter(user=user_id, name=portfolio_name).first()
+#         data = my_stocks.objects.filter(portfolio=my_portfolio).order_by('-name')
+#         new_list = []
+#         # for entry in data:
+#         #     new_list.append(entry.instrument_token)
+#         # print('test:', new_list)
+#         config = {
+#             "apiKey": "AIzaSyCU9JP2yixeKjw3NE30Pb0I0D0UQjV94gA",
+#             "authDomain": "kiteconnect-stock.firebaseapp.com",
+#             "databaseURL": "https://kiteconnect-stock-default-rtdb.firebaseio.com",
+#             "projectId": "kiteconnect-stock",
+#             "storageBucket": "kiteconnect-stock.appspot.com",
+#             "messagingSenderId": "981866107803",
+#             "appId": "1:981866107803:web:568ffc6b464656a6f4c73e"
+#             # measurementId: "G-9BRWTZYS5C"
+#         }
+#
+#         firebase = pyrebase.initialize_app(config)
+#         logging.basicConfig(level=logging.DEBUG)
+#         sym=[]
+#         total_qty=[]
+#         t_current_day_close_price=[]
+#         t_previous_day_close_price=[]
+#         for entry in data:
+#             sym.append(entry.tradingsymbol)
+#             quantity=entry.quantity
+#             current_day_close_price=firebase.database().child("Stock").child(entry.instrument_token).child("last_price").get().val()
+#             previous_day_close_price=firebase.database().child("Stock").child(entry.instrument_token).child("close").get().val()
+#             t_current_day_close_price.append(quantity*current_day_close_price)
+#             t_previous_day_close_price.append(quantity*previous_day_close_price)
+#
+#         print('test:', sym)
+#         print('test1:', total_qty)
+#         print('test2:', t_current_day_close_price)
+#         print('test3:', t_previous_day_close_price)
+#
+#         current_portfolio_value = sum(t_current_day_close_price)
+#         previous_day_portfolio_value =  sum(t_previous_day_close_price)
+#         portfolio_performance = (current_portfolio_value - previous_day_portfolio_value)/previous_day_portfolio_value
+#         print('portfolio_performance:',portfolio_performance*100)
+#         return Response("new_list")
+from django.db.models import Count
+import pandas as pd
+
+
+class DashboardApiSectorWiseData(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            auth = request.headers.get('Authorization')
+            token = Token.objects.get(key=auth.replace("Token ", ""))
+            user_id = token.user_id
+            portfolio_name = request.data['portfolio_name']
+            my_portfolio = portfolio.objects.filter(user=user_id, name=portfolio_name).first()
+            data = my_stocks.objects.filter(portfolio=my_portfolio).order_by('-name')
+
+            config = {
+                "apiKey": "AIzaSyCU9JP2yixeKjw3NE30Pb0I0D0UQjV94gA",
+                "authDomain": "kiteconnect-stock.firebaseapp.com",
+                "databaseURL": "https://kiteconnect-stock-default-rtdb.firebaseio.com",
+                "projectId": "kiteconnect-stock",
+                "storageBucket": "kiteconnect-stock.appspot.com",
+                "messagingSenderId": "981866107803",
+                "appId": "1:981866107803:web:568ffc6b464656a6f4c73e"
+            }
+            firebase = pyrebase.initialize_app(config)
+            logging.basicConfig(level=logging.DEBUG)
+            sym = []
+            t_current_day_last_price = []
+            dataa = {}
+            data_qty = {}
+            for entry in data:
+                sym.append(entry.tradingsymbol)
+                quantity = entry.quantity
+                current_day_last_price = firebase.database().child("Stock").child(entry.instrument_token).child(
+                    "last_price").get().val()
+                t_current_day_last_price.append(round((quantity * current_day_last_price), 2))
+                dataa[entry.tradingsymbol] = current_day_last_price
+                data_qty[entry.tradingsymbol] = quantity
+            # print('test:', sym)
+            df = pd.DataFrame.from_records(StockGeneralInfo.objects.filter(symbol__in=sym).values('sector', 'symbol'))
+            # print('try:', df)
+            group = df.groupby('sector')
+            df2 = group.apply(lambda x: x['symbol'].unique())
+            js = df2.to_json(orient='index')
+            res = json.loads(js)
+            # print('try:', res)
+            req_data = {}
+            for key, value in res.items():
+                s = 0
+                for i in value:
+                    # print('val:',i)
+                    dt = dataa[str(i)] * data_qty[str(i)]
+                    s = s + dt
+                    # print('vasssssl:', dt)
+                sector_percent = (s / sum(t_current_day_last_price)) * 100
+                req_data[key] = round(sector_percent, 2)
+            return Response(req_data, status=status.HTTP_200_OK)
+        except:
+            temp = [{"message": "data not found"}]
+            return Response(temp, status=status.HTTP_404_NOT_FOUND)
+
+
+class DashboardApiTesting(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        config = {
+            "apiKey": "AIzaSyCU9JP2yixeKjw3NE30Pb0I0D0UQjV94gA",
+            "authDomain": "kiteconnect-stock.firebaseapp.com",
+            "databaseURL": "https://kiteconnect-stock-default-rtdb.firebaseio.com",
+            "projectId": "kiteconnect-stock",
+            "storageBucket": "kiteconnect-stock.appspot.com",
+            "messagingSenderId": "981866107803",
+            "appId": "1:981866107803:web:568ffc6b464656a6f4c73e"
+        }
+        firebase = pyrebase.initialize_app(config)
+        logging.basicConfig(level=logging.DEBUG)
+
+        user = User.objects.all().values_list('id', flat=True)
+        for i in user:
+            my_portfolio = portfolio.objects.filter(user=i).values_list('id', flat=True)
+            for j in my_portfolio:
+                data = my_stocks.objects.filter(portfolio=j).order_by('-name')
+                print('data:', data)
+                sym = []
+                for entry in data:
+                    sym.append(entry.tradingsymbol)
+                print('asdada:', sym)
+
+            print("------------------")
+
+        return Response("ok", status=status.HTTP_200_OK)
